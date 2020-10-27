4d26a87a805306c78352a2f22a0cbc3fe86e31e5
TomaszKolodziejczak <64540133+TomaszKolodziejczak@users.noreply.github.com>
2020-10-27 12:19:55 +0100

    Add files via upload

---
+++ a_computer_builder.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from builder_pattern.abc_computer_builder import AbcComputerBuilder
+
+
+@dataclass
+class AComputerBuilder(AbcComputerBuilder):
+
+    def set_computer_version(self):
+        self._computer.version = "A"
+
+    def get_case(self):
+        self._computer.case = "Transparent Case"
+
+    def get_mainboard(self):
+        self._computer.mainboard = "XYZ 500"
+        self._computer.cpu = "AMD Ryzen"
+        self._computer.memory = "32GB"
+
+    def install_mainboard(self):
+        pass
+
+    def install_hard_drive(self):
+        self._computer.hard_disk = "Seagate 256GB"
---
+++ b_computer_builder.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+from builder_pattern.abc_computer_builder import AbcComputerBuilder
+
+
+@dataclass
+class BComputerBuilder(AbcComputerBuilder):
+
+    def set_computer_version(self):
+        """Set computer version"""
+        self._computer.version = "B"
+
+    def get_case(self):
+        self._computer.case = "Plastic Case"
+
+    def get_mainboard(self):
+        self._computer.mainboard = "alpha 100"
+        self._computer.cpu = "AMD"
+        self._computer.memory = "4GB"
+
+    def install_mainboard(self):
+        pass
+
+    def install_hard_drive(self):
+        self._computer.hard_disc = "64GB"
